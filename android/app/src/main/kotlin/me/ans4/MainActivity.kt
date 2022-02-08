060ecc4e6c268dca9f30d811ea6e169089c54cc2
Tahir Alvi <iamtahiralvi@gmail.com>
2022-02-08 21:47:50 +0000

    delet

--- android/app/src/main/kotlin/me/ans4/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package me.ans4
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/models/constants.dart
+++
@@ -1,6 +0,0 @@
-
-class Constants {
-  static final bookmarkTag = "bookmarked_list";
-  static final resentSearchTag = 'recent_search';
-  static final notificationTag = 'notifications';
-}
